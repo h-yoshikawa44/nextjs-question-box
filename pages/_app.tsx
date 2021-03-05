d3b7b77a409f3dd33b4fbf0f60ac8b90a1af7137
h-yoshikawa44 <guardian.yatagaras@icloud.com>
2021-03-05 13:28:49 +0900

    Chapter 12 - 受け取った質問一覧ページを作る

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,11 @@ import { RecoilRoot } from 'recoil'
 import '../lib/firebase'
 import '../styles/globals.scss'
 
+import dayjs from 'dayjs'
+import 'dayjs/locale/ja'
+
+dayjs.locale('ja')
+
 function MyApp({ Component, pageProps }) {
   return (
     <RecoilRoot>
