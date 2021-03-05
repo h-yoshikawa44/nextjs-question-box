f130306e55c16d7c59288405cc22dfdc33a34ac9
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2021-03-05 12:56:14 +0900

    Chapter 11 - 質問できるようにする

--- pages/users/[uid].tsx
+++ pages/users/[uid].tsx
@@ -1,6 +1,7 @@
 import { useRouter } from 'next/router'
-import { useEffect, useState } from 'react'
+import { FormEvent, useEffect, useState } from 'react'
 import firebase from 'firebase/app'
+import { toast } from 'react-toastify';
 import Layout from '../../components/Layout'
 import { User } from '../../models/User'
 
@@ -10,6 +11,8 @@ type Query = {
 
 export default function UserShow() {
   const [user, setUser] = useState<User>(null)
+  const [body, setBody] = useState('')
+  const [isSending, setIsSending] = useState(false)
   const router = useRouter()
   const query = router.query as Query
 
@@ -36,6 +39,33 @@ export default function UserShow() {
     loadUser()
   }, [query.uid])
 
+  async function onSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+
+    setIsSending(true)
+
+    await firebase.firestore().collection('questions').add({
+      senderUid: firebase.auth().currentUser.uid,
+      receiverUid: user.uid,
+      body,
+      isReplied: false,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+    })
+
+    setIsSending(false)
+
+    setBody('')
+    toast.success('質問を送信しました。', {
+      position: 'bottom-left',
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    })
+  }
+
   return (
     <Layout>
       {user && (
@@ -44,6 +74,30 @@ export default function UserShow() {
           <div className="m-5">{user.name}さんに質問しよう！</div>
         </div>
       )}
+      <div className="row justify-content-center mb-3">
+        <div className="col-12 col-md-6">
+          <form onSubmit={onSubmit}>
+            <textarea
+              className="form-control"
+              placeholder="おげんきですか？"
+              rows={6}
+              onChange={(e) => setBody(e.target.value)}
+              required
+            ></textarea>
+            <div className="m-3">
+              {isSending ? (
+                <div className="spinner-border text-secondary" role="status">
+                  <span className="visually-hidden">Loading...</span>
+                </div>
+              ) : (
+                <button type="submit" className="btn btn-primary">
+                  質問を送信する
+                </button>
+              )}
+            </div>
+          </form>
+        </div>
+      </div>
     </Layout>
   )
 }
